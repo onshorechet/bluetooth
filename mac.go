2cf839b863f45a1e5e5120d31ab8e40d6ecebebc
Chet Henry <chet.0x.henry@gmail.com>
2025-02-23 19:53:22 -0700

    Pull in temp changes from feature/mac and feature/uuid

--- mac.go
+++ mac.go
@@ -2,62 +2,14 @@ package bluetooth
 
 import (
 	"errors"
+	"strconv"
 	"unsafe"
+
+	"gopkg.in/yaml.v3"
 )
 
 // MAC represents a MAC address, in little endian format.
 type MAC [6]byte
-
-// ParseMAC parses the given MAC address, which must be in 11:22:33:AA:BB:CC
-// format. If it cannot be parsed, an error is returned.
-func ParseMAC(s string) (mac MAC, err error) {
-	err = (&mac).UnmarshalText([]byte(s))
-	return
-}
-
-func ParseAddress(s string) (add Address, err error) {
-	mac, err := ParseMAC(s)
-	if err != nil {
-		return Address{}, err
-	}
-
-	return Address{
-		MACAddress: MACAddress{
-			MAC:      mac,
-			isRandom: false,
-		},
-	}, nil
-}
-
-// String returns a human-readable version of this MAC address, such as
-// 11:22:33:AA:BB:CC.
-func (mac MAC) String() string {
-	buf, _ := mac.MarshalText()
-	return unsafe.String(unsafe.SliceData(buf), 17)
-}
-
-const hexDigit = "0123456789ABCDEF"
-
-// AppendText appends the textual representation of itself to the end of b
-// (allocating a larger slice if necessary) and returns the updated slice.
-func (mac MAC) AppendText(buf []byte) ([]byte, error) {
-	for i := 5; i >= 0; i-- {
-		if i != 5 {
-			buf = append(buf, ':')
-		}
-		buf = append(buf, hexDigit[mac[i]>>4])
-		buf = append(buf, hexDigit[mac[i]&0xF])
-	}
-	return buf, nil
-}
-
-// MarshalText marshals itself into a string of format 11:22:33:AA:BB:CC.
-// It is a simple wrapper of the AppentText method.
-func (mac MAC) MarshalText() (text []byte, err error) {
-	return mac.AppendText(make([]byte, 0, 17))
-}
-
-var ErrInvalidMAC = errors.New("bluetooth: failed to parse MAC address")
 
 // UnmarshalText unmarshals the text into itself.
 // The given MAC address byte array must be of the format 11:22:33:AA:BB:CC.
@@ -93,6 +45,43 @@ func (mac *MAC) UnmarshalText(s []byte) error {
 	return nil
 }
 
+// ParseMAC parses the given MAC address, which must be in 11:22:33:AA:BB:CC
+// format. If it cannot be parsed, an error is returned.
+func ParseMAC(s string) (mac MAC, err error) {
+	err = (&mac).UnmarshalText([]byte(s))
+	return
+}
+
+// String returns a human-readable version of this MAC address, such as
+// 11:22:33:AA:BB:CC.
+func (mac MAC) String() string {
+	buf, _ := mac.MarshalText()
+	return unsafe.String(unsafe.SliceData(buf), 17)
+}
+
+const hexDigit = "0123456789ABCDEF"
+
+// AppendText appends the textual representation of itself to the end of b
+// (allocating a larger slice if necessary) and returns the updated slice.
+func (mac MAC) AppendText(buf []byte) ([]byte, error) {
+	for i := 5; i >= 0; i-- {
+		if i != 5 {
+			buf = append(buf, ':')
+		}
+		buf = append(buf, hexDigit[mac[i]>>4])
+		buf = append(buf, hexDigit[mac[i]&0xF])
+	}
+	return buf, nil
+}
+
+// MarshalText marshals itself into a string of format 11:22:33:AA:BB:CC.
+// It is a simple wrapper of the AppentText method.
+func (mac MAC) MarshalText() (text []byte, err error) {
+	return mac.AppendText(make([]byte, 0, 17))
+}
+
+var ErrInvalidMAC = errors.New("bluetooth: failed to parse MAC address")
+
 // MarshalBinary marshals itself into a binary format.
 // This is a simple wrapper of the AppendBinary method
 func (mac MAC) MarshalBinary() (data []byte, err error) {
@@ -115,4 +104,16 @@ func (mac *MAC) UnmarshalBinary(data []byte) error {
 // (allocating a larger slice if necessary) and returns the updated slice.
 func (mac MAC) AppendBinary(b []byte) ([]byte, error) {
 	return append(b, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]), nil
+}
+
+func (mac MAC) MarshalYAML() (interface{}, error) {
+	return mac.String(), nil
+}
+
+func (mac *MAC) UnmarshalYAML(node *yaml.Node) error {
+	str, err := strconv.Unquote(string(node.Value))
+	if err != nil {
+		return mac.UnmarshalText([]byte(node.Value))
+	}
+	return mac.UnmarshalText([]byte(str))
 }
